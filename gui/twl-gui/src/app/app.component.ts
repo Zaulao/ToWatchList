aa46e3f1fe4e30f7ce70f15d95e07de736803cdb
Saulo Guilhermino Ferreira Lima <sgfl@ug3c20.windows.cin.ufpe.br>
2018-10-05 10:35:10 -0300

    elementos do cadastro de movies

--- gui/twl-gui/src/app/app.component.ts
+++ gui/twl-gui/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  movie: Movie = {name: "", priority:0};
+}
+export class Movie {
+  name: string;
+  priority: number;
 }
