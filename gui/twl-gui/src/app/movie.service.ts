d76b282df1d142148bc47aa123ca89c229ef631f
Zaulao <sgfl@cin.ufpe.br>
2018-10-05 11:05:19 -0300

    ordenação de array de filmes por prioridade implementada

--- gui/twl-gui/src/app/movie.service.ts
+++ gui/twl-gui/src/app/movie.service.ts
@@ -1,8 +1,9 @@
 import { Movie } from './movie';
 export class MovieService {
   movie: Movie[] = [];
-  
+
   gravar(movie: Movie): void {
      this.movie.push(movie);
+     this.movie.sort(function(a, b){return a.priority < b.priority ? 1:0});
   }
 }
